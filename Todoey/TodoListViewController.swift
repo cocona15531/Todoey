4ae747e98cdcc533c687d7f147dad1c9516eaf33
issei.ueda <ron.ron633662@gmail.com>
2024-01-17 18:55:43 +0900

    tableviewを追加して配列を表示させた

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//  2024/01/16
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["参考書を買う", "支払いをする", "更新する"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+    }
+
+
+    //MARK: - TableViewDatasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+
+
+}
+
