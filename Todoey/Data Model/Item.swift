d5f84c8aed16eb4e0e1845aaa25b71502b38b00f
issei.ueda <ron.ron633662@gmail.com>
2024-01-22 22:07:12 +0900

    データモデルを作成して、ファイリングをした

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,12 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Issei Ueda on 2024/01/22.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
