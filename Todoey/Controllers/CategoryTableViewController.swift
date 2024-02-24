056e409b0fceebc202c23731616cabf8568d2d67
issei.ueda <ron.ron633662@gmail.com>
2024-02-24 16:07:26 +0900

    タスクのカテゴリを新たに作成して、CoreDataでrelationshipを作成した

---
+++ Todoey/Controllers/CategoryTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey
+//
+//  Created by Issei Ueda on 2024/02/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+    
+    var cate
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+}
