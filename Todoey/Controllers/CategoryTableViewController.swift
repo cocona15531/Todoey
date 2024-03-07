a25a68d037cfa4b01c87233e34ed391a29f97a5a
issei.ueda <ron.ron633662@gmail.com>
2024-03-07 16:28:06 +0900

    スワイプでカテゴリーを削除できるようにした

--- Todoey/Controllers/CategoryTableViewController.swift
+++ Todoey/Controllers/CategoryTableViewController.swift
@@ -118,13 +118,29 @@ extension CategoryTableViewController: SwipeTableViewCellDelegate {
         guard orientation == .right else { return nil }
 
         let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
-            print("Item deleted")
+            //            print("Item deleted")
+            if let categoryForDeletion = self.categories?[indexPath.row] {
+                do {
+                    try self.realm.write {
+                        self.realm.delete(categoryForDeletion)
+                    }
+                } catch {
+                    print("\(error)")
+                }
+                
+            }
         }
 
         // customize the action appearance
         deleteAction.image = UIImage(named: "delete-icon")
 
         return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
     }
     
     
